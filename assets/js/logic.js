1b1559043e81c7bb011c151c8e6ea438a9ddbe0d
Sheetal <sheetal.srikumar@gmail.com>
2020-03-08 23:15:46 -0700

    options click working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,48 +9,75 @@ var feedbackEl = document.getElementById("feedback");
 var currentQuestionIndex = 0;
 
 function startQuiz() {
-    // hide start screen
-    var startScreenEl = document.getElementById("start-screen");
-    startScreenEl.setAttribute("class", "hide");
-  
-    // un-hide questions section
-    questionsEl.removeAttribute("class");
-  
-    // start timer
-    //timerId = setInterval(clockTick, 1000);
-  
-    // show starting time
-    //timerEl.textContent = time;
-  
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
+
+  // start timer
+  //timerId = setInterval(clockTick, 1000);
+
+  // show starting time
+  //timerEl.textContent = time;
+
+  getQuestion();
+}
+
+function getQuestion() {
+  // get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
+
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
+
+  // loop over choices
+  currentQuestion.choices.forEach(function (choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+startBtn.onclick = startQuiz;
+
+function questionClick() {
+  if (this.value != [currentQuestionIndex].answer) {
+    feedbackEl.textContent = "Wrong";
+
+
+  }
+  else {
+
+    feedbackEl.textContent = "Right";
+  }
+  // flash right/wrong feedback on page for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  // move to next question
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
     getQuestion();
   }
-  
-  function getQuestion() {
-    // get current question object from array
-    var currentQuestion = questions[currentQuestionIndex];
-  
-    // update title with current question
-    var titleEl = document.getElementById("question-title");
-    titleEl.textContent = currentQuestion.title;
-  
-    // clear out any old question choices
-    choicesEl.innerHTML = "";
-  
-    // loop over choices
-    currentQuestion.choices.forEach(function(choice, i) {
-      // create new button for each choice
-      var choiceNode = document.createElement("button");
-      choiceNode.setAttribute("class", "choice");
-      choiceNode.setAttribute("value", choice);
-  
-      choiceNode.textContent = i + 1 + ". " + choice;
-  
-      // attach click event listener to each choice
-      choiceNode.onclick = questionClick;
-  
-      // display on the page
-      choicesEl.appendChild(choiceNode);
-    });
-  }
 
-  startBtn.onclick = startQuiz;
+}
